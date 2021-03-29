0884c678b12b7db2ef8fe3f99afe1ac943ca5cef
danielwright1207 <daniel.wright1207@gmail.com>
2021-03-29 13:35:17 -0600

    heroku push

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-// const server = require("./api/server.js");
-
-// const PORT = process.env.PORT || 8000;
-// server.listen(PORT, () => {
-//   console.log(`\n=== Server listening on port ${PORT} ===\n`);
-// });
 require("dotenv").config();
 
 const server = require("./api/server");
--- knexfile.js
+++ knexfile.js
@@ -1,29 +1,55 @@
 // Update with your config settings.
+// require("dotenv").config();
+// const pg = require("pg");
+// if (process.env.DATABASE_URL) {
+//   pg.defaults.ssl = { rejectUnauthorized: false };
+// }
+// const sharedConfig = {
+//   client: "pg",
+//   migrations: { directory: "./data/migrations" },
+// };
 
+// module.exports = {
+//   development: {
+//     ...sharedConfig,
+//     connection: process.env.DEV_DATABASE_URL,
+//     // connection: { filename: "./data/secret-family-recipes.db3" },
+//     seeds: { directory: "./data/seeds" },
+//   },
+//   testing: {
+//     ...sharedConfig,
+//     connection: process.env.TESTING_DATABASE_URL,
+//     // connection: { filename: "./data/test.db3" },
+//   },
+//   production: {
+//     client: "pg",
+//     connection: process.env.DATABASE_URL,
+//     migrations: { directory: "./data/migrations" },
+//     seeds: { directory: "./data/seeds" },
+//   },
+// };
+require("dotenv").config();
+const pg = require("pg");
+if (process.env.DATABASE_URL) {
+  pg.defaults.ssl = { rejectUnauthorized: false };
+}
 const sharedConfig = {
-  client: "sqlite3",
-  useNullAsDefault: true,
+  client: "pg",
   migrations: { directory: "./data/migrations" },
-  pool: {
-    afterCreate: (conn, done) => conn.run("PRAGMA foreign_keys = ON", done),
-  },
+  seeds: { directory: "./data/seeds" },
 };
-
 module.exports = {
   development: {
     ...sharedConfig,
-    connection: { filename: "./data/secret-family-recipes.db3" },
-    seeds: { directory: "./data/seeds" },
+    connection: process.env.DEV_DATABASE_URL,
   },
   testing: {
     ...sharedConfig,
-    connection: { filename: "./data/test.db3" },
+    connection: process.env.TESTING_DATABASE_URL,
   },
   production: {
-    client: "pg",
-    useNullAsDefault: true,
+    ...sharedConfig,
     connection: process.env.DATABASE_URL,
-    migrations: { directory: "./data/migrations" },
-    seeds: { directory: "./data/seeds" },
+    pool: { min: 2, max: 10 },
   },
 };
