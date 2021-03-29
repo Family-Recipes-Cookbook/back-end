fd065546bb043af48fe5b32fbca36451d7979d24
danielwright1207 <daniel.wright1207@gmail.com>
2021-03-29 11:40:15 -0600

    error troubleshooting

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const server = require("./api/server.js");
+// const server = require("./api/server.js");
 
-const PORT = process.env.PORT || 8000;
-server.listen(PORT, () => {
-  console.log(`\n=== Server listening on port ${PORT} ===\n`);
+// const PORT = process.env.PORT || 8000;
+// server.listen(PORT, () => {
+//   console.log(`\n=== Server listening on port ${PORT} ===\n`);
+// });
+require("dotenv").config();
+
+const server = require("./api/server");
+const port = process.env.PORT || 8000;
+
+server.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
 });
