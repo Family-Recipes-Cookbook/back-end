5a0a175ef0afa9008640a3c919bce5190cd69c12
danielwright1207 <daniel.wright1207@gmail.com>
2021-03-29 14:17:25 -0600

    still working with postgress

--- knexfile.js
+++ knexfile.js
@@ -1,33 +1,3 @@
-// Update with your config settings.
-// require("dotenv").config();
-// const pg = require("pg");
-// if (process.env.DATABASE_URL) {
-//   pg.defaults.ssl = { rejectUnauthorized: false };
-// }
-// const sharedConfig = {
-//   client: "pg",
-//   migrations: { directory: "./data/migrations" },
-// };
-
-// module.exports = {
-//   development: {
-//     ...sharedConfig,
-//     connection: process.env.DEV_DATABASE_URL,
-//     // connection: { filename: "./data/secret-family-recipes.db3" },
-//     seeds: { directory: "./data/seeds" },
-//   },
-//   testing: {
-//     ...sharedConfig,
-//     connection: process.env.TESTING_DATABASE_URL,
-//     // connection: { filename: "./data/test.db3" },
-//   },
-//   production: {
-//     client: "pg",
-//     connection: process.env.DATABASE_URL,
-//     migrations: { directory: "./data/migrations" },
-//     seeds: { directory: "./data/seeds" },
-//   },
-// };
 require("dotenv").config();
 const pg = require("pg");
 if (process.env.DATABASE_URL) {
